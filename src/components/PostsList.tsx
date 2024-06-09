d534dbd49cf0141d73c2314f24a4cd91e5da949f
braverokmc79 <braverokmc79@gmail.com>
2024-06-09 15:58:36 +0900

    백인드 추가 및 useEffect() 부수적 효과 처리

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,31 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 
-
 export type PostType = {
-  id:string;
+  id: string;
   author: string;
   body: string;
 };
 
-interface PostsListProps{
+interface PostsListProps {
   isPosting: boolean;
   setModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
   hideModalHandler: () => void;
 }
 
-const initializeData={
-  id:"",
-  author: "", 
-  body: "" 
-}
+const initializeData = {
+  id: "",
+  author: "",
+  body: "",
+};
 
-const PostsList:React.FC<PostsListProps>= (props) => {
+const PostsList: React.FC<PostsListProps> = (props) => {
   const [posts, setPosts] = useState<PostType[]>([]);
   const [newPost, setNewPost] = useState<PostType>(initializeData);
- 
+
   const postDataChangerHandler = (key: string, value: string) => {
     setNewPost((prevNewPost) => ({
       ...prevNewPost,
@@ -33,22 +32,52 @@ const PostsList:React.FC<PostsListProps>= (props) => {
     }));
   };
 
-  const addPostHandler = (postData:PostType) => {
-    setPosts((existingPosts)=>[postData, ...existingPosts]);
+
+  useEffect(() => {
+    try{
+      const postList = async () => {
+        const respose = await fetch("http://localhost:8080/posts");
+        const resData = await respose.json();
+        setPosts(resData.posts);
+      };
+
+      
+      postList();
+    }catch(error){
+      console.log("에러 : ",error);
+    }
+
+  }, []);
+
+
+
+  const addPostHandler = (postData: PostType) => {
+    const addPost = async () => {
+      await fetch("http://localhost:8080/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      });
+    };
+
+    addPost();
+
+    setPosts((existingPosts) => [postData, ...existingPosts]);
     setNewPost(initializeData); // 초기화
     props.setModalVisible(false);
   };
 
-  const onStopPostion=()=>{
+  const onStopPostion = () => {
     props.hideModalHandler();
     setNewPost(initializeData); // 초기화
-  }
-
+  };
 
   let modalConent;
-  if(props.isPosting){
+  if (props.isPosting) {
     modalConent = (
-      <Modal onClose={props.hideModalHandler}   >
+      <Modal onClose={props.hideModalHandler}>
         <NewPost
           newPost={newPost}
           onPostChange={postDataChangerHandler}
@@ -60,16 +89,17 @@ const PostsList:React.FC<PostsListProps>= (props) => {
   }
 
   return (
-    <>     
+    <>
       {modalConent}
-     {posts.length >0 && <ul>
+      {posts.length > 0 && (
+        <ul>
           {posts.map((post) => (
             <Post key={post.id} author={post.author} body={post.body} />
           ))}
         </ul>
-      }
-      {posts.length ===0 &&(
-        <div style={{textAlign:'center', color:'white'}}>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
           <h2>등록된 포스트가 없습니다.</h2>
           <p>포스트를 추가해 주세요!</p>
         </div>
